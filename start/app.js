96aab512672b142270ee48e980547dc32a6a9cdf
icld <76025734+icld@users.noreply.github.com>
2021-02-16 16:36:36 -0800

    added edit and save capabilities.

--- start/app.js
+++ start/app.js
@@ -4,7 +4,9 @@ const ul = document.getElementById('invitedList');
 
 function createLI(text) {
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span)
     const label = document.createElement('label');
     label.textContent = 'Confirmed'
     const checkbox = document.createElement('input');
@@ -50,11 +52,24 @@ ul.addEventListener('click', (e) => {
         const li = button.parentNode;
         const ul = li.parentNode;
         if (button.textContent === 'remove') {
-
             ul.removeChild(li);
             console.log('removed')
         } else if (button.textContent === 'edit') {
             console.log('edit')
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'save';
+        } else if (button.textContent === 'save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span')
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit'
         }
     }
 
